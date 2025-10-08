b8a639dde1423dfa0b77aa018d7b7089f6795ce4
nikita.armeec <nikita.armeec.88@gmail.com>
2025-10-08 22:37:44 +0300

    first test my llm

--- src/main/java/ru/kxnvg/myllm/MyLlmApplication.java
+++ src/main/java/ru/kxnvg/myllm/MyLlmApplication.java
@@ -1,13 +1,17 @@
 package ru.kxnvg.myllm;
 
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class MyLlmApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyLlmApplication.class, args);
+        ConfigurableApplicationContext context = SpringApplication.run(MyLlmApplication.class, args);
+        ChatClient chatClient = context.getBean(ChatClient.class);
+        System.out.println(chatClient.prompt().user("Дай первую строчку богемской рапсодии").call().content());
     }
 
 }
