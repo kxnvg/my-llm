91391f44958d688755329a588e041317d91a97cc
nikita.armeec <nikita.armeec.88@gmail.com>
2025-10-08 21:29:22 +0300

    init project with base config

---
+++ src/main/java/ru/kxnvg/myllm/MyLlmApplication.java
@@ -0,0 +1,13 @@
+package ru.kxnvg.myllm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLlmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLlmApplication.class, args);
+    }
+
+}
